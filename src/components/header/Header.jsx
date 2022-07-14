a3ab9d2d43fa5d7d46a71c6e11598e06c6a057d1
mehmet <tas349873@gmail.com>
2022-07-14 20:49:47 +0300

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,18 @@
-import HeaderStyle from './Header.style';
+import React from 'react'
+import HeaderStyle, { Button, FoodInput, FormContainer, HeaderContainer, MainHeader, Select } from './Header.style'
+const Header = ({setQuery}) => {
+  return (
+    <HeaderContainer>
+      <MainHeader>Recipe App</MainHeader>
+      <FormContainer>
+        <FoodInput type="text" placeholder='search' onChange={(e)=>setQuery(e.target.value)}>
 
-const Header = () => {
-  return <div>Header</div>;
-};
+        </FoodInput>
+        <Button type='submit'>SEARCH</Button>
+        <Select name='mealtype' id='mealType' ></Select>
+      </FormContainer>
+    </HeaderContainer>
+  )
+}
 
-export default Header;
+export default Header
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,37 @@
-import HomeStyle from './Home.style';
+import HomeStyle from "./Home.style";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  const mealTypes=["Breakfast","Lunch","Dinner","Snack","Teatime"]
+  const [query, setQuery] = useState("egg");
+  const [selectedMeal, setSelectedMeal] = useState("breakfast");
+  const [recipes, setRecipes] = useState("");
+  const APP_ID = `ebe4dc0a`;
+  const APP_KEY = `bd2b4d21cff9784aa4ababb3fd7bf856`;
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=$
+  {APP_KEY}&mealType=${selectedMeal}`;
+
+  const getData = async () => {
+    try {
+      //! {data} => Destructring
+      const { data } = await axios.get(url);
+      setRecipes(data.hits);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <Header setQuery={setQuery} setSelectedMeal={setSelectedMeal} />
+    </div>
+  );
 };
 
 export default Home;
